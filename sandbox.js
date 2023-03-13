d1395e07c2247058c14ba3927bf74c2fb8ab0a1c
Margi <margipancholi7@gmail.com>
2023-03-12 21:37:12 -0400

    template string

--- sandbox.js
+++ sandbox.js
@@ -48,3 +48,41 @@
 
 // let index = email.indexOf('@');
 // console.log('index of the @ sign:', index);
+// common string methods
+
+let email = 'mario@thenetninja.co.uk';
+
+//let result = email.lastIndexOf('n');
+
+//let result = email.slice(0,5);
+
+//let result = email.substr(5,12);
+
+//let result = email.replace('m', 'w');
+
+let result = email.replace('n', 'w');
+
+console.log(result);
+// template strings
+const title = 'Best reads of 2019';
+const author = 'Mario';
+const likes = 30;
+
+// concatenation way
+
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+// console.log(result);
+
+// template string way
+
+// let result = `The blog called ${title} by ${author} has ${likes} likes`;
+// console.log(result);
+
+// creating html templates
+let html = `
+  <h2>${title}</h2>
+  <p>By ${author}</p>
+  <span>This blog has ${likes} likes</span>
+`;
+
+console.log(html);
