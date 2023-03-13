e9154776aa3b5022ed7bcf59336d0ef80aad2815
Margi <margipancholi7@gmail.com>
2023-03-12 21:34:26 -0400

    ch2

---
+++ sandbox.js
@@ -0,0 +1,50 @@
+// // //alert('this is ch1 day1');
+// // let king =100 ; 
+// // king =10;
+// // console.log(king);
+
+// // const k=12;
+// // console.log(k);
+// //  //k =16;
+// // console.log(k);
+// let age = 25;
+// let year = 2019;
+
+// console.log(age, year);
+
+// // age = 30;
+// // console.log(age);
+
+// // const points = 100;
+// // points = 50;
+// // console.log(points);
+
+// // var score = 75;
+// // console.log(score);
+// // strings
+// console.log('hello, world');
+
+// let email = 'mario@thenetninja.co.uk';
+// console.log(email);
+
+// // string concatenation
+// let firstName = 'Brandon';
+// let lastName = 'Sanderson';
+
+// let fullName = firstName + ' ' + lastName;
+
+// console.log(fullName);
+
+// // getting individual characters
+// console.log(fullName[2]);
+
+// // string length
+// console.log(fullName.length);
+
+// // string methods
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLocaleLowerCase();
+// console.log(result);
+
+// let index = email.indexOf('@');
+// console.log('index of the @ sign:', index);
