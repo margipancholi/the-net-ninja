35b1f80aca0ba1637c640aff108715d9ec42c2ca
Margi <margipancholi7@gmail.com>
2023-03-13 13:30:57 -0400

    newrepo

--- sandbox.js
+++ sandbox.js
@@ -1,93 +1,93 @@
-// // // //alert('this is ch1 day1');
-// // // let king =100 ; 
-// // // king =10;
-// // // console.log(king);
+// // // // //alert('this is ch1 day1');
+// // // // let king =100 ; 
+// // // // king =10;
+// // // // console.log(king);
 
-// // // const k=12;
-// // // console.log(k);
-// // //  //k =16;
-// // // console.log(k);
-// // let age = 25;
-// // let year = 2019;
+// // // // const k=12;
+// // // // console.log(k);
+// // // //  //k =16;
+// // // // console.log(k);
+// // // let age = 25;
+// // // let year = 2019;
 
-// // console.log(age, year);
+// // // console.log(age, year);
 
-// // // age = 30;
-// // // console.log(age);
+// // // // age = 30;
+// // // // console.log(age);
 
-// // // const points = 100;
-// // // points = 50;
-// // // console.log(points);
+// // // // const points = 100;
+// // // // points = 50;
+// // // // console.log(points);
 
-// // // var score = 75;
-// // // console.log(score);
-// // // strings
-// // console.log('hello, world');
+// // // // var score = 75;
+// // // // console.log(score);
+// // // // strings
+// // // console.log('hello, world');
+
+// // // let email = 'mario@thenetninja.co.uk';
+// // // console.log(email);
+
+// // // // string concatenation
+// // // let firstName = 'Brandon';
+// // // let lastName = 'Sanderson';
+
+// // // let fullName = firstName + ' ' + lastName;
+
+// // // console.log(fullName);
+
+// // // // getting individual characters
+// // // console.log(fullName[2]);
+
+// // // // string length
+// // // console.log(fullName.length);
+
+// // // // string methods
+// // // console.log(fullName.toUpperCase());
+// // // let result = fullName.toLocaleLowerCase();
+// // // console.log(result);
+
+// // // let index = email.indexOf('@');
+// // // console.log('index of the @ sign:', index);
+// // // common string methods
 
 // // let email = 'mario@thenetninja.co.uk';
-// // console.log(email);
 
-// // // string concatenation
-// // let firstName = 'Brandon';
-// // let lastName = 'Sanderson';
+// // //let result = email.lastIndexOf('n');
 
-// // let fullName = firstName + ' ' + lastName;
+// // //let result = email.slice(0,5);
 
-// // console.log(fullName);
+// // //let result = email.substr(5,12);
 
-// // // getting individual characters
-// // console.log(fullName[2]);
+// // //let result = email.replace('m', 'w');
 
-// // // string length
-// // console.log(fullName.length);
+// // let result = email.replace('n', 'w');
 
-// // // string methods
-// // console.log(fullName.toUpperCase());
-// // let result = fullName.toLocaleLowerCase();
 // // console.log(result);
+// // // template strings
+// // const title = 'Best reads of 2019';
+// // const author = 'Mario';
+// // const likes = 30;
 
-// // let index = email.indexOf('@');
-// // console.log('index of the @ sign:', index);
-// // common string methods
+// // // concatenation way
 
-// let email = 'mario@thenetninja.co.uk';
+// // // let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+// // // console.log(result);
 
-// //let result = email.lastIndexOf('n');
+// // // template string way
 
-// //let result = email.slice(0,5);
+// // // let result = `The blog called ${title} by ${author} has ${likes} likes`;
+// // // console.log(result);
 
-// //let result = email.substr(5,12);
+// // // creating html templates
+// // let html = `
+// //   <h2>${title}</h2>
+// //   <p>By ${author}</p>
+// //   <span>This blog has ${likes} likes</span>
+// // `;
 
-// //let result = email.replace('m', 'w');
+// // console.log(html);
+// // null & undefined
+// let age = null;
 
-// let result = email.replace('n', 'w');
-
-// console.log(result);
-// // template strings
-// const title = 'Best reads of 2019';
-// const author = 'Mario';
-// const likes = 30;
-
-// // concatenation way
-
-// // let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
-// // console.log(result);
-
-// // template string way
-
-// // let result = `The blog called ${title} by ${author} has ${likes} likes`;
-// // console.log(result);
-
-// // creating html templates
-// let html = `
-//   <h2>${title}</h2>
-//   <p>By ${author}</p>
-//   <span>This blog has ${likes} likes</span>
-// `;
-
-// console.log(html);
-// null & undefined
-let age = null;
-
-console.log(age, age + 3, `the age is ${age}`);
+// console.log(age, age + 3, `the age is ${age}`);
 
